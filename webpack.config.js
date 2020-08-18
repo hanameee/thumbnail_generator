4db81232c22d4b456f29e1d7b0d686c0190dda4a
hanameee <hanameee@gmail.com>
2020-08-19 00:11:28 +0900

    [Init] Install webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,14 @@ const path = require("path");
 module.exports = {
     mode: "development",
     entry: {
-        main: "./src/App.js",
+        main: "./src/index.html",
     },
     output: {
         filename: "[name].js",
         path: path.resolve("./dist"),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 8080,
     },
 };
