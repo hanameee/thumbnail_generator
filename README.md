0e6c49bed226777413ba3c8f3c5e0c046489b5a2
hanameee <hanameee@gmail.com>
2020-08-21 01:05:20 +0900

    [Init] Typescript+React configuration
    - Installed dependency for ts and react
    - Added tsconfig
    - Added HtmlWebpackPlugin
    - Modified existing component to tsx

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# thumbnail_generator
+# Thumbnail Generator 🎆
+
+## 1. 환경 설정
+
+### 1-1. Webpack 설정
+
+Webpack을 사용해 React, Typescript를 사용하는 SPA 환경설정을 해보자.
+
+1) 새로운 프로젝트 생성
+
+```bash
+mkdir 프로젝트이름
+cd 프로젝트이름
+npm init -y
+mkdir src
+touch src/index.tsx
+```
+
+2)  Dependencies 설치
+
+Runtime Dependencies는 다음과 같다.
+
+- react
+- react-dom
+
+Build Dependencies는 다음과 같다.
+
+- webpack
+- webpack-cli
+- typescript
+- @types/react
+- @types/react-dom
+- @types/webpack
+- ts-loader
+
+
+
+[Reference]
+
+- [React + Typescript + Webpack](https://dev.to/jacopobonta/react-typescript-webpack-3c6l)
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
     entry: {
-        main: "./src/index.html",
+        main: path.join(__dirname, "src", "index.tsx"),
+    },
+    target: "web",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: "/node_modules/",
+            },
+        ],
     },
     output: {
         filename: "[name].js",
-        path: path.resolve("./dist"),
+        path: path.resolve(__dirname, "dist"),
     },
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         port: 8080,
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+    ],
 };
