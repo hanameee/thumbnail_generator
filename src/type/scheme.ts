3dc933fea08886c6abc9a55a1a5a12a24d5a0968
hanameee <hanameee@gmail.com>
2020-09-16 23:53:20 +0900

    [Feat] Add color palette interface
    - Based on material UI theme properties

---
+++ src/type/scheme.ts
@@ -0,0 +1,32 @@
+interface IPalette {
+    primary: {
+        main: string;
+        light: string;
+        dark: string;
+    };
+    secondary: {
+        main: string;
+        light: string;
+        dark: string;
+    };
+    error: {
+        main: string;
+        light: string;
+        dark: string;
+    };
+    warning: {
+        main: string;
+        light: string;
+        dark: string;
+    };
+    info: {
+        main: string;
+        light: string;
+        dark: string;
+    };
+    success: {
+        main: string;
+        light: string;
+        dark: string;
+    };
+}
